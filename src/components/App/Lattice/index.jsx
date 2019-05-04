fcd70edef274663dda820be06feec7c7b272eafb
Lajos.Meszaros <m_lajos@hotmail.com>
2019-05-04 19:58:07 +0200

    The Lattice cursor now takes the color of an existing blob

--- src/components/App/Lattice/index.jsx
+++ src/components/App/Lattice/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { compose, reject, intersection, isEmpty, length } from 'ramda'
+import React, { useState } from 'react'
+import { compose, reject, intersection, isEmpty, length, pathOr } from 'ramda'
 import { connect } from 'react-redux'
 import { getPressedNotesFromNoteTable } from '../../../helpers/MIDI'
 import { getRelativeCoordinates } from './helpers'
@@ -12,23 +12,20 @@ const enhance = compose(
     height: state.lattice.height,
     url: state.lattice.url,
     blobs: state.lattice.blobs,
-    pressedKeys: getPressedNotesFromNoteTable(state.midi.noteTable)
+    pressedKeys: getPressedNotesFromNoteTable(state.midi.noteTable),
+    nextColor: 'purple'
   }))
 )
 
-const Lattice = ({ width, height, url, blobs, pressedKeys }) => {
+const Lattice = ({ width, height, url, blobs, pressedKeys, nextColor }) => {
   const visibleBlobs = reject(({ assignedMidiKeys }) => isEmpty(intersection(pressedKeys, assignedMidiKeys)), blobs)
 
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 })
   const [isCursorOverLattice, setIsCursorOverLattice] = useState(false)
-  const [isOnlyOneNotePressed, setIsOnlyOneNotePressed] = useState(false)
+  const isOnlyOneNotePressed = length(pressedKeys) === 1
   const isCursorVisible = isCursorOverLattice && isOnlyOneNotePressed
-  const cursorColor = 'yellow'
+  const cursorColor = pathOr(nextColor, [0, 'color'], visibleBlobs)
   const cursorSize = 30
-
-  useEffect(() => {
-    setIsOnlyOneNotePressed(length(pressedKeys) === 1)
-  }, [pressedKeys])
 
   return (
     <div
